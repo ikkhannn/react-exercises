f3057b07525c07d264427dfaee93ecf7ad9350b4
Imran Khan <imrankhannn81@gmail.com>
2020-10-17 22:50:18 +0500

    conditions ifelse and ternary

--- my-app/src/Ninjas.js
+++ my-app/src/Ninjas.js
@@ -2,17 +2,36 @@ import React from 'react';
 
 const Ninjas = ({ninjas}) => {
 
+    // const ninjaList = ninjas.map(ninja =>{
+    //     if(ninja.age>30){
+    //     return (
+    //         <div className="ninja" key={ninja.id}>
+    //         <div>{ninja.name}</div>
+    //         <div>{ninja.age}</div>
+    //         <div>{ninja.belt}</div>
+
+    //     </div>
+ 
+    //     )
+    //     } else{
+    //         return null;
+    //     }
+
+    // });
+
+
     const ninjaList = ninjas.map(ninja =>{
 
-        return (
+        return ninja.age>30? (
+        
             <div className="ninja" key={ninja.id}>
-            <div>{ninja.name}</div>
-            <div>{ninja.age}</div>
-            <div>{ninja.belt}</div>
+                <div>{ninja.name}</div>
+                <div>{ninja.age}</div>
+                <div>{ninja.belt}</div>
 
-        </div>
- 
-        )
+            </div>
+                
+        ):null;
 
     });
     return(
