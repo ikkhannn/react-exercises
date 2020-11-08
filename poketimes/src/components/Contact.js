882c5ac508b82fb50ead23afa37602dfe679e75a
Imran Khan <imrankhannn81@gmail.com>
2020-11-08 19:35:02 +0500

    programming redirects

---
+++ poketimes/src/components/Contact.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Contact = (props) => {
+    // setTimeout(()=>{
+    //     props.history.push('/about')
+    // },2000);
+    console.log(props);
+    return(
+        <div className="container">
+            <h4 className="center">Contact</h4>
+            <p>lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum </p>
+        </div>
+    )
+}
+
+export default Contact;
---
+++ poketimes/src/components/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Link,NavLink,withRouter} from 'react-router-dom';
+
+const Navbar = (props) => {
+    // setTimeout(() => {
+    //     props.history.push('/about')
+    // },2000);
+    return(
+        <nav className="nav wrapper red darken-3">
+            <div className="container">
+                <a className="brand-logo" href="#">Poketimes</a>
+                <ul className="right"> 
+                    <li><NavLink to="/">Home</NavLink></li>
+                    <li><NavLink to="/about">About</NavLink></li>
+                    <li><NavLink to="/contact">contact</NavLink></li>
+                </ul>
+            </div>
+        </nav>
+    );
+    
+}
+
+export default withRouter(Navbar);
